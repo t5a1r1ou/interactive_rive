ce30a6749267103f0c046392ee7fd4375c53a0fb
t5a1r1ou <baw.keepon69@gmail.com>
2023-02-09 16:13:37 +0900

    修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
-window.addEventListener("load", () => {
-  const r = new rive.Rive({
-    src: "./assets/rivs/bird_interactive.riv",
-    canvas: document.getElementById("canvas"),
-    autoplay: true,
-    stateMachines: "state",
-    fit: rive.Fit.cover,
-    onLoad: (_) => {
-      const inputs = r.stateMachineInputs('state');
-      console.log(inputs);
-    },
-  });
+new rive.Rive({
+  src: "./assets/rivs/bird_interactive.riv",
+  canvas: document.getElementById("canvas"),
+  autoplay: true,
+  stateMachines: "state",
 });
