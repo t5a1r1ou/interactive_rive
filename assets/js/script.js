9077eeda2c90019585e7b50ae1bcaaaa9878c292
t5a1r1ou <baw.keepon69@gmail.com>
2023-02-09 16:12:25 +0900

    修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-new rive.Rive({
-  src: "./assets/rivs/bird_interactive.riv",
-  canvas: document.getElementById("canvas"),
-  autoplay: true
+window.addEventListener("load", () => {
+  const r = new rive.Rive({
+    src: "./assets/rivs/bird_interactive.riv",
+    canvas: document.getElementById("canvas"),
+    autoplay: true,
+    stateMachines: "state",
+    fit: rive.Fit.cover,
+    onLoad: (_) => {
+      const inputs = r.stateMachineInputs('state');
+      console.log(inputs);
+    },
+  });
 });
